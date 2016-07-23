cf352b1db76358a602be865ed76a3fce115eb3b8
Sebastian Thiel <byronimo@gmail.com>
2016-07-23 17:06:21 +0100

    Update documentation

--- implementors/core/str/pattern/trait.ReverseSearcher.js
+++ implementors/core/str/pattern/trait.ReverseSearcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["serde"] = [];implementors["libc"] = [];implementors["openssl"] = [];
+implementors["libc"] = [];implementors["lazy_static"] = [];implementors["serde"] = [];implementors["openssl"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
